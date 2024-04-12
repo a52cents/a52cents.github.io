9d176a6cff51e560f8768cb518ba8fd7ff9ce108
a52cents <96891277+a52cents@users.noreply.github.com>
2024-04-12 16:30:48 +0200

    Contact page

--- my-app/app/contact/page.jsx
+++ my-app/app/contact/page.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import styles from './contact.module.css'
+import Image from 'next/image'
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src="/contact.png" alt='alt'fill  />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder='Nom et Prénom'/>
+          <input type="text" placeholder='Adresse mail'/>
+          <input type="text" placeholder='Téléphone(optional)'/>
+          <textarea
+            name=''
+            id=''
+            cols={30}
+            rows={10}
+            placeholder='Message'>
+            </textarea>
+          <button>Envoyer</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
