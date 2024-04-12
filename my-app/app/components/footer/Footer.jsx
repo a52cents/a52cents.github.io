9d3874076656836ee1b56b20f736fd7780688af2
a52cents <96891277+a52cents@users.noreply.github.com>
2024-04-12 08:40:29 +0200

    Footer

--- my-app/app/components/footer/Footer.jsx
+++ my-app/app/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import styles from './footer.module.css'
 const Footer = () => {
   return (
-    <div>
-    
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        @yaskaa
+      </div>
+      <div className={styles.text}>
+        blog of yaskaa © All rights reserved.
+      </div>
     </div>
   )
 }
